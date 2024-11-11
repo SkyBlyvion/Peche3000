ee8c6d3b8cc9b47e3e884a1486045a8da6f8ef08
SkyBlyvion <richtofen60@gmail.com>
2024-11-11 15:44:10 +0100

    add first entity Produit.java with the Categorie.java enum and DateCreation = LocalDateTime.now() within the constructor

---
+++ src/main/java/com/magasinpeche/MagasinPecheApplication.java
@@ -0,0 +1,22 @@
+package com.magasinpeche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class MagasinPecheApplication implements WebMvcConfigurer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagasinPecheApplication.class, args);
+    }
+
+    // customizes how Spring serves static resources
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Permet de servir les fichiers du dossier uploads
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+    }
+}
